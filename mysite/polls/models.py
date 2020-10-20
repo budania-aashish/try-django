bc4ffa3e2c0e09fd8a549885dea4c96b71c5a47b
Aashish <aashish@Spinnys-MacBook-Pro.local>
2020-10-20 18:40:25 +0530

    added some modular functions

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    # custom method to check if the question was added recently
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -11,3 +20,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
