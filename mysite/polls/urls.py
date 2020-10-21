544c29c28eef0e90d87452869014b3b082a71ed9
Aashish <aashish@Spinnys-MBP.domain.name>
2020-10-21 10:19:52 +0530

    added few views and url patterns

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,24 @@
 from django.urls import path, include
 from django.contrib import admin
 from . import views
-
 urlpatterns = [
+    # to tell the application to include polls
     path('polls/', include('polls.urls')),
+
+    # for the admin page
     path('admin/', admin.site.urls),
+
+    # ex /polls/
     path('', views.index, name='index'),
+
+    # ex /polls/5
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # ex: /polls/5/results
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    # ex: /polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
+
